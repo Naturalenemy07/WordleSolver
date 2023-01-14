891ee1e61178adb8eda54d56eae7d31611216f8d
John Caruthers <john.d.caruthers@gmail.com>
2023-01-13 22:40:11 -0500

    will update chromedriver, imported checkdriver

--- wordle_solver.py
+++ wordle_solver.py
@@ -4,15 +4,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from wordle_logic import WordleSolver as ws
+from checkdriver import Checker
 import numpy as np
 
 # Get wordlist and create game
-wordlist = np.loadtxt("<path to enhancedwordlist.txt>", dtype='str')
-starting_words = np.array(["crane", "slate", "least", "dealt"])
+pathToCDexe = 'path\to\chromedriver.exe'
+pathToCD = 'path\to\chromedriver_win32'
+wordlist = np.loadtxt("path\to\enhancedwordlist.txt", dtype='str')
+starting_words = np.array(["reach", "dealt", "salet", "crane", "audio"])
 game = ws(starting_words, wordlist)
 
+# Update Chromedriver as needed
+ckr = Checker(pathToCD)
+
 # Start up automated bot
-s = Service('<path to chromedriver.exe>')
+s = Service(pathToCDexe)
 browser = webdriver.Chrome(service=s)
 url = 'https://www.nytimes.com/games/wordle/index.html'
 browser.get(url)
@@ -24,14 +30,14 @@ exit_button.click()
 time.sleep(2)
 
 # find interactable element
-interactable = browser.find_element(By.CLASS_NAME, 'Key-module_key__Rv-Vp')
+interactable = browser.find_element(By.CLASS_NAME, 'Key-module_key__kchQI')
 browser.implicitly_wait(10)
 
 def type_in_word(word):
     # hard coded to type in 5 letters similar to human, press enter
     for i in word:
             interactable.send_keys(i)
-            time.sleep(0.1)
+            time.sleep(0.2)
     time.sleep(1)
     interactable.send_keys(Keys.ENTER)
     time.sleep(3)
@@ -41,7 +47,10 @@ def check_status(row):
     # interacts with wordle html and finds status of each letter with by passing to WordleSolver.get_status
     word_status_list = []
     for let_pos in range(1,6):
-        status_input = str(browser.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[1]/div/div['+str(row)+']/div['+str(let_pos)+']/div').get_attribute('outerHTML'))
+        try:
+            status_input = str(browser.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[1]/div/div['+str(row)+']/div['+str(let_pos)+']/div').get_attribute('outerHTML'))
+        except:
+            status_input = str(browser.find_element(By.XPATH, '/html/body/div/div/div/div/div[1]/div/div['+str(row)+']/div['+str(let_pos)+']/div').get_attribute('outerHTML'))
         browser.implicitly_wait(10)
         word_status_list.append(game.return_aria_label(status_input))
     return word_status_list
